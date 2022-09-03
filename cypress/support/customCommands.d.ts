6c22957bed0e202b7e5782a6d4c4b96ecccf75f0
Felipe Galdino <felipegaldino21@gmail.com>
2022-09-03 14:55:29 -0300

    testes de api

--- cypress/support/customCommands.d.ts
+++ cypress/support/customCommands.d.ts
@@ -2,5 +2,7 @@ declare namespace Cypress {
     interface Chainable<Subject> {
         login(): Chainable<any>
         logout(): Chainable<any>
+        gui_createProject(project: any): Chainable<any>
+        gui_createIssue(issue: any): Chainable<any>
   }
 }
